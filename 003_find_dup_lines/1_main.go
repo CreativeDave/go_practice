1b7af1fa1a7243c1ea60954af84c6a528f99c3f8
david wilson <wilson11235813@gmail.com>
2020-02-20 23:33:23 -0600

    added an exercise repo, 003_find_dup_lines

---
+++ 003_find_dup_lines/1_main.go
@@ -0,0 +1,23 @@
+// Prints the text of each line that appears more than once,
+// preceded by its count.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts :=make(map[string]int)
+	input :=bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Println("%d\t%s\n", n, line)
+		}
+	}
+}
